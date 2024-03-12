88064311199669356409413e6a338f393676d2f3
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-03-11 23:39:41 -0500

    Valores y Variables

--- app/src/main/java/com/example/androidmaster/Variables.kt
+++ app/src/main/java/com/example/androidmaster/Variables.kt
@@ -1,2 +1,31 @@
 package com.example.androidmaster
 
+//VARIABLES
+
+fun main(){
+
+    //Variables numericas
+
+    //Int -2.147.483.648 to 2.147.483.647
+    val age:Int = 40
+    var age2:Int = 30
+    age2 = 31
+    //Long -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807
+    val example:Long = 1000000000000000000
+    val heigth:Float = 1.81f
+    val double:Double = 321.123
+
+
+    //Variables Alfanumericas
+
+    //Char
+    val letter:Char = 'A'
+    val name:String = "Ronaldinho"
+
+
+    //Variables Booleanas
+    val isTrue:Boolean = true
+    val isFalse:Boolean = false
+
+
+}
